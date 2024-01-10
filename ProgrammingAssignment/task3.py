fda1421046a14e288d4770a56c8a117a2884bc30
alexh <alex.h.friedrich@gmail.com>
2024-01-10 13:29:51 +0100

    Added docstrings to all fcts in task3.py
    changed running times to actually only show the measure the network was used for
    
    implemented hyperparameter tuning: one model with 2 hidden layers, one with 3, as well as 6 differing hidden layer sizes (maybe also include learning rate, but everything converges kind of alright so it is just additional computation)
    
    updated the methods to include weights if there are any, i.e. second network
    
    updated plot_approximation_ratio to include the measure being computed in the title
    
    Should there be higher input dim when weighted? I'm very uncertain atm and don't want to fuck anything up, let's discuss that later.

--- ProgrammingAssignment/task3.py
+++ ProgrammingAssignment/task3.py
@@ -16,25 +16,33 @@ import pandas as pd
 
 # read in Graph data for Pytorch Geometric
 def read_graph(path):
+    # Read in the graph
     return nx.read_adjlist(path, delimiter=' ', nodetype=int, comments='%')
 
 
-paths = ['./data/moreno_propro/out.moreno_propro_propro',
-         'data/moreno_health/out.moreno_health_health']
-G = read_graph(paths[0])
-
-# Initialize an empty dictionary to store the running times
-running_times = {}
-
 def calc_targets(graph):
+    """
+    :param graph:
+    :return: a list of targets
+    """
     centrality_measures = ['degree_centrality', 'eigenvector_centrality', 'page_rank']
     functions = [nx.degree_centrality, nx.eigenvector_centrality, nx.pagerank]
     targets = []
-    
+
     for measure, function in zip(centrality_measures, functions):
         start_time = time.time()
+
         if measure == 'eigenvector_centrality':
-            result = function(graph, max_iter=1000)
+            if len(list(graph.edges)[0]) == 3:
+                print(graph.weight)
+                result = function(graph, weight='weight', max_iter=1000)
+            else:
+                result = function(graph, max_iter=1000)
+        elif measure == 'page_rank':
+            if len(list(graph.edges)[0]) == 3:
+                result = function(graph, weight='weight')
+            else:
+                result = function(graph)
         else:
             result = function(graph)
         running_time = time.time() - start_time
@@ -48,6 +56,11 @@ def calc_targets(graph):
 
 
 def create_dataset(g, targets):
+    '''
+    :param g: networkx graph
+    :param targets: dictionary of centrality measures
+    :return: a PyTorch Geometric Data object
+    '''
     # Extract node indices and corresponding degree centrality values
     x, y = zip(*[(node, targets[node]) for node in g.nodes])
 
@@ -72,6 +85,10 @@ def create_dataset(g, targets):
 
 
 def create_mask(train_data):
+    '''
+    :param train_data: PyTorch Geometric Data object
+    Set training mask to 90% of the data
+    '''
     random_integers = np.random.randint(0, len(train_data.y), size=int((len(train_data.y) / 10)))
     train_mask = torch.full_like(train_data.y, True, dtype=bool)
     train_mask[random_integers] = False
@@ -79,12 +96,24 @@ def create_mask(train_data):
 
 
 def preprocessing(path):
+    """
+    :param path: path to the graph
+    :return: a list of PyTorch Geometric Data objects
+    """
     g = read_graph(path)
-
-    targets = calc_targets(g)
 
     G_networkx = nx.Graph()
     G_networkx.add_edges_from(g.edges)
+
+    if len(list(g.edges)[0]) == 3:
+        print("weighted")
+        G_networkx = nx.read_weighted_edgelist(path, delimiter=' ', nodetype=int, comments='%')
+
+    if nx.is_directed(G_networkx):
+        G_networkx = nx.read_edgelist(path, delimiter=' ', nodetype=int, comments='%')
+
+    targets = calc_targets(g)
+
     train_data_sets = [create_dataset(G_networkx, target) for target in targets]
 
     for data_set in train_data_sets:
@@ -92,32 +121,65 @@ def preprocessing(path):
 
     return train_data_sets
 
-def plot_approximation_ratio(data, test_out):
+
+def plot_approximation_ratio(data, test_out, measure=""):
+    """
+    :param data: PyTorch Geometric Data object
+    :param test_out: predicted values
+    :param measure: centrality measure
+    Produce a histogram of the approximation ratios
+    """
     eps = 1e-8  # small constant to avoid division by 0
     approx_ratios = test_out.view(-1) / (data.y + eps)
     plt.hist(approx_ratios.numpy(), bins=50, alpha=0.7, color='blue', label='Approximation Ratios')
     plt.axvline(x=1, color='red', linestyle='--', label='True Value')
     plt.xlabel('Approximation Ratio')
     plt.ylabel('Frequency')
-    plt.title('Distribution of Approximation Ratios')
+    plt.title(f'Distribution of Approximation Ratios - {measure}')
     plt.legend()
     plt.show()
 
+
 def compute_kendall_tau(data, test_out):
+    """
+    :param data: PyTorch Geometric Data object
+    :param test_out: predicted values
+    Compute Kendall's tau between the true and predicted values
+    """
     # Compute the rankings of the true and predicted values
     true_ranking = np.argsort(data.y.numpy())
     predicted_ranking = np.argsort(test_out.view(-1).numpy())
 
     # Compute Kendall's tau
-    tau, p_value = kendalltau(true_ranking, predicted_ranking)
+    tau, _ = kendalltau(true_ranking, predicted_ranking)
 
     print(f"Kendall's tau: {tau}")
 
-# Define a model
-class GNN(nn.Module):
+
+class GNN_3(nn.Module):
+
     def __init__(self, in_channels, hidden_channels, out_channels):
-        super(GNN, self).__init__()
+        super(GNN_3, self).__init__()
         self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, out_channels)
+
+    def forward(self, x, edge_index):
+        x = self.conv1(x, edge_index).relu()
+        x = self.conv2(x, edge_index).relu()
+        x = self.conv3(x, edge_index)
+        return x
+
+
+class GNN_2(nn.Module):
+    '''
+    A simple GNN with two GCNConv layers
+    '''
+
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GNN_2, self).__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+
         self.conv2 = GCNConv(hidden_channels, out_channels)
 
     def forward(self, x, edge_index):
@@ -127,9 +189,20 @@ class GNN(nn.Module):
         return output
 
 
-def process(model, data, num_steps=100, lr=0.01):
+def process(model, data, num_steps=100, lr=0.01, tune=False, measure_int=-1):
+    """
+    :param model: GNN model
+    :param data: PyTorch Geometric Data object
+    :param num_steps: number of training steps
+    :param lr: learning rate
+
+    Train the model and evaluate it on the test set
+    """
     criterion = nn.L1Loss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
+    # store train and test error in a dictionary
+    if tune:
+        errors = {'test_error': []}
 
     for epoch in range(num_steps):
         model.train()
@@ -143,9 +216,11 @@ def process(model, data, num_steps=100, lr=0.01):
 
         running_loss += loss.item()
 
-    print('Training finished.')
-    train_loss = criterion(out.view(-1)[data.train_mask], data.y[data.train_mask])
-    print(f'Train Loss: {train_loss.item()}')
+    if not tune:
+        print('Training finished.')
+        train_loss = criterion(out.view(-1)[data.train_mask], data.y[data.train_mask])
+
+        print(f'Train Loss: {train_loss.item()}')
     model.eval()
     with torch.no_grad():
         start_time = time.time()
@@ -154,19 +229,75 @@ def process(model, data, num_steps=100, lr=0.01):
         running_times['gnn_method'] = gnn_time
 
         test_loss = criterion(test_out.view(-1), data.y)
-        print(f'Test Loss: {test_loss.item()}')
+        if tune:
+            errors['test_error'].append(test_loss.item())
+        else:
+            print(f'Test Loss: {test_loss.item()}')
+            if measure_int >= 0:
+                measure = list(running_times.keys())[measure_int]
 
-        plot_approximation_ratio(data, test_out)
-        compute_kendall_tau(data, test_out)
+            plot_approximation_ratio(data, test_out, measure)
+            compute_kendall_tau(data, test_out)
+    if tune:
+        return errors
 
-def print_running_times():
+
+def print_running_times(measure):
+    """
+    Print the running times of the different methods
+    """
     df = pd.DataFrame(running_times, index=['Running Time'])
-    print(df)
+    cols = df.columns
+    print(df[[cols[measure], cols[-1]]])
 
-for p in paths:
-    data_sets = preprocessing(p)
-    for train_data in data_sets:
-        model = GNN(in_channels=1, hidden_channels=128, out_channels=1)
-        process(model, train_data, num_steps=500)
-        print_running_times()
 
+def create_model(config, model_type=GNN_2):
+    """
+    :param config: dictionary of model parameters
+    :param model_type: GNN model
+    :return: GNN model
+    """
+    m = model_type(config['in_channels'], config['hidden_channels'], config['out_channels'])
+    return m
+
+
+if __name__ == '__main__':
+    # Set the random seed for reproducibility
+    torch.manual_seed(0)
+    paths = ['./data/moreno_propro/out.moreno_propro_propro',
+             'data/moreno_health/out.moreno_health_health']
+
+    # Initialize an empty dictionary to store the running times
+    running_times = {}
+
+    # create a variety of different models with differing hidden sizes for each of GNN_2 and GNN_3
+    configs = [{'in_channels': 1, 'hidden_channels': 16, 'out_channels': 1},
+               {'in_channels': 1, 'hidden_channels': 32, 'out_channels': 1},
+               {'in_channels': 1, 'hidden_channels': 64, 'out_channels': 1},
+               {'in_channels': 1, 'hidden_channels': 128, 'out_channels': 1},
+               {'in_channels': 1, 'hidden_channels': 256, 'out_channels': 1},
+               {'in_channels': 1, 'hidden_channels': 512, 'out_channels': 1}]
+
+    models_2 = [(create_model(config, model_type=GNN_2), config) for config in configs]
+    models_3 = [(create_model(config, model_type=GNN_3), config) for config in configs]
+
+    models = models_2 + models_3
+    for p in paths:
+        data_sets = preprocessing(p)
+        for i, train_data in enumerate(data_sets):
+            # store train and test errors of different configs in a dictionary
+            errors = {'test_error': []}
+            for model in models:
+                res = process(model[0], train_data, num_steps=100, tune=True)
+                errors['test_error'].append(res['test_error'])
+
+            # choose the model with the lowest test error
+            best_model = models[np.argmin(errors['test_error'])]
+            if best_model[0].__class__.__name__ == 'GNN_2':
+                print('GNN_2')
+            else:
+                print('GNN_3')
+            print(best_model[1])
+
+            process(best_model[0], train_data, num_steps=100, measure_int=i)
+            print_running_times(measure=i)
